60fd4cdf27fc078108f67dcbcf340bb6982f01b4
huynh1234 <huynhphoke@gmail.com>
2021-05-10 17:03:07 +0700

    loading gif

--- label_studio/frontend/src/components/Spinner/Spinner.js
+++ label_studio/frontend/src/components/Spinner/Spinner.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { cn } from '../../utils/bem';
 import './Spinner.styl';
 import Page from 'react-page-loading'
+import GifLoader from 'react-gif-loader';
+
+
 
 export const Spinner = ({ className, style, size, stopped = false }) => {
   const rootClass = cn('spinner');
@@ -9,10 +12,16 @@ export const Spinner = ({ className, style, size, stopped = false }) => {
   return (
     <div className={rootClass.mix(className)} style={{...(style ?? {}), '--spinner-size': `${size ?? 32}px`}}>
       <div className={rootClass.elem('body').mod({stopped}) } >
-     
-        <Page loader={"spin"} color={"#A9A9A9"} size={6}>
-        </Page>
-    
+        <div style={{width:"11em",height:"11em"}}>
+          <GifLoader
+                loading={true}
+                imageSrc="https://i.postimg.cc/D0fxLkv1/Vanilla-1s-288px.gif"
+                imageStyle={{marginTop: "0%",width:"8em",heigth:"8em"}}
+                overlayBackground="null"
+            />
+        </div>
+        
+      
       </div>
     </div>
     // <div></div>
