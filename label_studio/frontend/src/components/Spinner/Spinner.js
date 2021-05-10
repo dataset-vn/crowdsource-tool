65627aa14323f638b531ae7e7247f9372f07c73d
huynh1234 <huynhphoke@gmail.com>
2021-05-10 22:18:54 +0700

    load rotate-spin

--- label_studio/frontend/src/components/Spinner/Spinner.js
+++ label_studio/frontend/src/components/Spinner/Spinner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { cn } from '../../utils/bem';
 import './Spinner.styl';
 import Page from 'react-page-loading'
-import GifLoader from 'react-gif-loader';
 
 
 
@@ -12,14 +11,8 @@ export const Spinner = ({ className, style, size, stopped = false }) => {
   return (
     <div className={rootClass.mix(className)} style={{...(style ?? {}), '--spinner-size': `${size ?? 32}px`}}>
       <div className={rootClass.elem('body').mod({stopped}) } >
-        <div style={{width:"11em",height:"11em"}}>
-          <GifLoader
-                loading={true}
-                imageSrc="https://i.postimg.cc/D0fxLkv1/Vanilla-1s-288px.gif"
-                imageStyle={{marginTop: "0%",width:"8em",heigth:"8em"}}
-                overlayBackground="null"
-            />
-        </div>
+      <Page loader={"rotate-spin"} color={"#A9A9A9"} size={6}>
+        </Page>
         
       
       </div>
