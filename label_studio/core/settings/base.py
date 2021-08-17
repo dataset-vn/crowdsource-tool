41b37734c44e2f46f0765e6c0811e9d068ebc77d
Nguyen Thinh <ng.tr.thinh@gmail.com>
2021-08-17 18:00:10 +0700

    Translation for Vietnamese, Django part

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -276,11 +276,22 @@ GRAPHIQL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi'
 TIME_ZONE = 'UTC'
-USE_I18N = False
+USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES = (
+  # ('en', _('English')),
+  ('vi', _('Vietnamese')),
+)
+
+LOCALE_PATHS = (
+  os.path.join(BASE_DIR, 'locale'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
