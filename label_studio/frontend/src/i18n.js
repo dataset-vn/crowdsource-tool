76582c9670aef75fb36ff8d1e7c0bd6fed69285d
Nguyen Thinh <ng.tr.thinh@gmail.com>
2021-08-18 11:03:01 +0700

    React error

---
+++ label_studio/frontend/src/i18n.js
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import TRANSLATIONS_VI from "./translations/vi/translation.json";
+import TRANSLATIONS_EN from "./translations/en/translation.json";
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en: {
+				translation: TRANSLATIONS_EN,
+			},
+			vi: {
+				translation: TRANSLATIONS_VI,
+			},
+		},
+	});
+var locale = navigator.language;
+if (locale == "vi") i18n.changeLanguage("vi");
+else i18n.changeLanguage("en");
