03e12839bb2d203e9df51b0ca0851bb0e76ae4d7
Quoc Hung <hungtnq62@wru.vn>
2021-09-10 13:02:23 +0700

    demo Public Project Dashboard

--- label_studio/frontend/src/pages/Projects/ProjectsList.js
+++ label_studio/frontend/src/pages/Projects/ProjectsList.js
@@ -56,10 +56,45 @@ const ProjectCard = ({project, history}) => {
       '--background-color': chr(color).alpha(0.2).css(),
     } : {};
   }, [color]);
+  const projectstatus = 'Complete';
+  const userstatus = 'Available';
+  const projectType = 'Community';
+
+  const projectStatus = useMemo(() => {
+    var style = {};
+    projectstatus == 'Recuiting' ? 
+    style = {
+      'background': '#E8E8E8',
+      'color': '#003FFF',
+      'height': '30px',
+      'text-align': 'center',
+      'padding-top': '10px'
+    }
+    :
+      projectstatus == 'Working' ?
+      style = {
+        'background': '#E8E8E8',
+        'color': '#22E000',
+        'height': '30px',
+        'text-align': 'center',
+        'padding-top': '10px'
+      }
+      :
+      style = {
+        'background': '#E8E8E8',
+        'color': "#C4C4C4",
+        'height': '30px',
+        'text-align': 'center',
+        'padding-top': '10px'
+      }
+    return style;
+  });
+
   const { t } = useTranslation();
   return (
     <Elem tag={NavLink} name="link" to={`/projects/${project.id}/data`} data-external>
       <Block name="project-card" mod={{colored: !!color}} style={projectColors}>
+        <Elem name="project-status" style={projectStatus}>{projectstatus}</Elem>
         <Elem name="header">
           <Elem name="title">
             <Elem name="title-text">
@@ -81,25 +116,40 @@ const ProjectCard = ({project, history}) => {
             </Elem>
           </Elem>
           <Elem name="summary">
-            <Elem name="annotation">
-              <Elem name="total">
-                {project.num_tasks_with_annotations} / {project.task_number}
-              </Elem>
-              <Elem name="detail">
-                <Elem name="detail-item" mod={{type: "completed"}}>
-                  <Elem tag={LsCheck} name="icon"/>
-                  {project.total_annotations_number}
+            { projectstatus == 'Recuiting' ? (
+              <Elem name="annotation">
+                <Elem name="total">
+                  {projectType}
                 </Elem>
-                <Elem name="detail-item" mod={{type: "rejected"}}>
-                  <Elem tag={LsMinus} name="icon"/>
-                  {project.skipped_annotations_number}
-                </Elem>
-                <Elem name="detail-item" mod={{type: "predictions"}}>
-                  <Elem tag={LsBulb} name="icon"/>
-                  {project.total_predictions_number}
+                <Elem name="detail">
+                  {userstatus}
                 </Elem>
               </Elem>
-            </Elem>
+            ) 
+            :
+            projectstatus == 'Working' ? (
+                <Elem name="annotation">
+                  <Elem name="total">
+                    {project.num_tasks_with_annotations} / {project.task_number}
+                  </Elem>
+                  <Elem name="detail">
+                    <Elem name="detail-item" mod={{type: "completed"}}>
+                      <Elem tag={LsCheck} name="icon"/>
+                      {project.total_annotations_number}
+                    </Elem>
+                    <Elem name="detail-item" mod={{type: "rejected"}}>
+                      <Elem tag={LsMinus} name="icon"/>
+                      {project.skipped_annotations_number}
+                    </Elem>
+                    <Elem name="detail-item" mod={{type: "predictions"}}>
+                      <Elem tag={LsBulb} name="icon"/>
+                      {project.total_predictions_number}
+                    </Elem>
+                  </Elem>
+                </Elem>
+              )
+              : null
+            }
           </Elem>
         </Elem>
         <Elem name="description">
