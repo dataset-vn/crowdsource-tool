1ed207f1067da0abb227b635780ccc61883018d1
Nguyen Thinh <ng.tr.thinh@gmail.com>
2021-09-23 21:28:15 +0700

    add translations for project detail

--- label_studio/frontend/src/pages/Projects/ProjectDetail.js
+++ label_studio/frontend/src/pages/Projects/ProjectDetail.js
@@ -4,8 +4,8 @@ import { ProjectContext, useProject } from "../../providers/ProjectProvider";
 import { useCurrentUser } from "../../providers/CurrentUser";
 import { useAPI } from "../../providers/ApiProvider";
 import { Block, Elem } from "../../utils/bem";
-import { Space } from '../../components/Space/Space';
-import { Button } from '../../components/Button/Button';
+import { Space } from "../../components/Space/Space";
+import { Button } from "../../components/Button/Button";
 import { Redirect } from "react-router-dom";
 import { useParams as useRouterParams } from "react-router";
 import {
@@ -13,87 +13,98 @@ import {
 	IconMoney,
 	IconGroupOfPersons,
 } from "../../assets/icons";
-
+import { useTranslation } from "react-i18next";
 
 export const ProjectDetailPage = () => {
 	const { project, fetchProject } = useContext(ProjectContext);
-		const color = useMemo(() => {
-			return project.color === "#FFFFFF" ? null : project.color;
-		}, [project]);
+	const color = useMemo(() => {
+		return project.color === "#FFFFFF" ? null : project.color;
+	}, [project]);
 
-		const projectColors = useMemo(() => {
-			return color
-				? {
-						"--header-color": color,
-						"--background-color": chr(color).alpha(0.2).css(),
-				}
-				: {};
-		}, [color]);
+	const projectColors = useMemo(() => {
+		return color
+			? {
+					"--header-color": color,
+					"--background-color": chr(color).alpha(0.2).css(),
+			  }
+			: {};
+	}, [color]);
 
-		return (
-			<Block name='project-detail-page'>
-				<Elem name='content' case='loaded'>
-					<Block
-						name='detail-card'
-						mod={{ colored: !!color }}
-						style={projectColors}>
-						<Elem name='header'>
-							<Elem name='title'>{project.title}</Elem>
-						</Elem>
-						<Elem name='stats_with_nums'>
-							<Elem name='stat-item'>
-								{project.project_type == "profitable" ? (
-									<Elem name='profitable_project'>
-										<Elem name='project_income'>Income:</Elem>
-										<Elem name='numtype-data-withicon'>
-											{project.project_rate}
-											<Elem tag={IconMoney} name='icon' />
-										</Elem>
+	const { t } = useTranslation();
+
+	return (
+		<Block name='project-detail-page'>
+			<Elem name='content' case='loaded'>
+				<Block
+					name='detail-card'
+					mod={{ colored: !!color }}
+					style={projectColors}>
+					<Elem name='header'>
+						<Elem name='title'>{project.title}</Elem>
+					</Elem>
+					<Elem name='stats_with_nums'>
+						<Elem name='stat-item'>
+							{project.project_type == "profitable" ? (
+								<Elem name='profitable_project'>
+									<Elem name='project_income'>
+										{t("projectDetail.incomeLabel")}
 									</Elem>
-								) : (
-									<Elem name='nonprofit_project'>
-										Community Project - Nonprofit
+									<Elem name='numtype-data-withicon'>
+										{project.project_rate}
+										<Elem tag={IconMoney} name='icon' />
 									</Elem>
-								)}
-							</Elem>
-							<Elem name='stat-item'>
-								<Elem name='project_size-label'>Expected Project Size:</Elem>
-								<Elem name='numtype-data-withicon'>
-									{project.project_size}
-									<Elem tag={IconGroupOfPersons} name='icon' />
-								</Elem>
-							</Elem>
-							<Elem name='stat-item'>
-								<Elem name='project_due-label'>Expected Due Date:</Elem>
-								<Elem name='numtype-data-withicon'>
-									{project.project_due}
-									<Elem tag={IconCalendar} name='icon' />
-								</Elem>
-							</Elem>
-						</Elem>
-						<Elem name='description'>
-							<Elem name='description-label'>Project Description: </Elem>
-							<Elem name='description-data'>
-								{project.description
-									? project.description
-									: "(This project has no description yet.)"}
-							</Elem>
-						</Elem>
-						<Elem name='current_user_role'>
-							{project.current_user_role ? (
-								<Elem name='user_role_sentence'>
-									You are currently a(an) {project.current_user_role}
 								</Elem>
 							) : (
-								<Elem name='user_role_sentence'>
-									You haven't joined this project yet
+								<Elem name='nonprofit_project'>
+									{t("projectDetail.communityProjectLabel")}
 								</Elem>
 							)}
 						</Elem>
-					</Block>
-				</Elem>
-			</Block>
-		);
+						<Elem name='stat-item'>
+							<Elem name='project_size-label'>
+								{t("projectDetail.projectSizeLabel")}
+							</Elem>
+							<Elem name='numtype-data-withicon'>
+								{project.project_size}
+								<Elem tag={IconGroupOfPersons} name='icon' />
+							</Elem>
+						</Elem>
+						<Elem name='stat-item'>
+							<Elem name='project_due-label'>
+								{t("projectDetail.dueDateLabel")}
+							</Elem>
+							<Elem name='numtype-data-withicon'>
+								{project.project_due}
+								<Elem tag={IconCalendar} name='icon' />
+							</Elem>
+						</Elem>
+					</Elem>
+					<Elem name='description'>
+						<Elem name='description-label'>
+							{t("projectDetail.descriptionLabel")}
+						</Elem>
+						<Elem name='description-data'>
+							{project.description
+								? project.description
+								: t("projectDetail.noDescription")}
+						</Elem>
+					</Elem>
+					<Elem name='current_user_role'>
+						{project.current_user_role ? (
+							<Elem name='user_role_sentence'>
+								{t("projectDetail.currentRoleLabel")}
+								{project.current_user_role}
+							</Elem>
+						) : (
+							<Elem name='user_role_sentence'>
+								{t("projectDetail.noRoleLabel")}
+							</Elem>
+						)}
+					</Elem>
+				</Block>
+			</Elem>
+		</Block>
+	);
 };
 
 ProjectDetailPage.title = "Details";
@@ -102,69 +113,82 @@ ProjectDetailPage.path = "/details";
 
 ProjectDetailPage.context = () => {
 	const { project } = useProject();
-	if(project && project.hasOwnProperty('current_user_role') && project.hasOwnProperty('id')) {
-	  var userRole = project.current_user_role;
-	  var projectID = project.id;
+	if (
+		project &&
+		project.hasOwnProperty("current_user_role") &&
+		project.hasOwnProperty("id")
+	) {
+		var userRole = project.current_user_role;
+		var projectID = project.id;
 	}
 	const { user } = useCurrentUser();
-	if(user && user.hasOwnProperty('id')) {
-	  var userID = user.id;
+	if (user && user.hasOwnProperty("id")) {
+		var userID = user.id;
 	}
 	const api = useAPI();
 	const createProjectMember = async () => {
-	  const response = await api.callApi("createProjectMember", {
-		params: {
-		  pk: projectID
-		},
-		body: {
-		  user_pk: userID,
-		  role: "pending"
-		}
-	  })
+		const response = await api.callApi("createProjectMember", {
+			params: {
+				pk: projectID,
+			},
+			body: {
+				user_pk: userID,
+				role: "pending",
+			},
+		});
 		window.location.reload();
-	}
-  
+	};
+
 	const removeProjectMember = async () => {
-	  const response = await api.callApi("removeProjectMember", {
-		params: {
-		  pk: projectID
-		},
-		body: {
-		  user_pk: userID,
+		const response = await api.callApi("removeProjectMember", {
+			params: {
+				pk: projectID,
+			},
+			body: {
+				user_pk: userID,
+			},
+		});
+
+		if (response.code == 200) {
+			window.location.reload();
 		}
-	  });
-  
-	  if (response.code == 200) {
-		window.location.reload();
-	  }
-	}
-  
+	};
+
+	const { t } = useTranslation();
+
 	return project && project.id ? (
-	  <Space size="small">
-		{(project.expert_instruction && mode !== 'explorer') && (
-		  <Button size="compact" onClick={() => {
-			modal({
-			  title: t('labelInstruc.title'),
-			  body: () => <div dangerouslySetInnerHTML={{__html: project.expert_instruction}}/>,
-			});
-		  }}>
-			{t('labelInstruc.action')}
-		  </Button>
-		)}
-		{userRole == null ? 
-		(
-		  <Button onClick={createProjectMember} style={{color: "white", background: "#0D88BC"}}>Apply</Button>
-		)
-		: 
-		userRole == "pending" ?
-		(
-		  <Button onClick={removeProjectMember} style={{color: "white", background: "#BC0D17"}}>Cancel</Button>
-		)
-		:
-		userRole == "trainee" ? null
-		: 
-		null
-		}
-	  </Space>
+		<Space size='small'>
+			{project.expert_instruction && mode !== "explorer" && (
+				<Button
+					size='compact'
+					onClick={() => {
+						modal({
+							title: t("labelInstruc.title"),
+							body: () => (
+								<div
+									dangerouslySetInnerHTML={{
+										__html: project.expert_instruction,
+									}}
+								/>
+							),
+						});
+					}}>
+					{t("labelInstruc.action")}
+				</Button>
+			)}
+			{userRole == null ? (
+				<Button
+					onClick={createProjectMember}
+					style={{ color: "white", background: "#0D88BC" }}>
+					{t("projectDetail.applyButtonLabel")}
+				</Button>
+			) : userRole == "pending" ? (
+				<Button
+					onClick={removeProjectMember}
+					style={{ color: "white", background: "#BC0D17" }}>
+					{t("projectDetail.cancelButtonLabel")}
+				</Button>
+			) : userRole == "trainee" ? null : null}
+		</Space>
 	) : null;
-  };
+};
