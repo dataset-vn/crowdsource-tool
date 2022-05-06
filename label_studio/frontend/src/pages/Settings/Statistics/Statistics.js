8693fb00a9ba46359a133dd175d312e8739e3d56
vumathuat <longvule070402@gmail.com>
2022-05-06 14:20:16 +0700

    statistics

--- label_studio/frontend/src/pages/Settings/Statistics/Statistics.js
+++ label_studio/frontend/src/pages/Settings/Statistics/Statistics.js
@@ -1,13 +1,17 @@
 import { useMemo, useState } from "react";
 import { Card, Button } from "react-bootstrap";
+import Table from 'react-bootstrap/Table'
 import { useTranslation } from "react-i18next";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Statistics.css';
 
 export const Statistics = () => {
 
   return (
   <div>
-  <Card border="primary" style={{ width: '18rem' }}>
+  <section className="Card">
+  <h5 style={{fontFamily: 'Inter', fontWeight: '700'}}> THỐNG KÊ TOÀN DỰ ÁN </h5>
+  <Card bg="primary" style={{width: '16rem',  textAlign: 'center'}}>
     <Card.Header>Tổng số tác vụ cần nhãn dán</Card.Header>
     <Card.Body>
       <Card.Title>5435435</Card.Title>
@@ -15,7 +19,7 @@ export const Statistics = () => {
   </Card>
   <br />
 
-  <Card border="danger" style={{ width: '18rem' }}>
+  <Card bg="danger" style={{width: '16rem',  textAlign: 'center'}}>
     <Card.Header>Tổng số tác vụ đã dán nhãn</Card.Header>
     <Card.Body>
       <Card.Title>12335</Card.Title>
@@ -23,7 +27,7 @@ export const Statistics = () => {
   </Card>
   <br />
 
-  <Card border="warning" style={{ width: '18rem' }}>
+  <Card bg="warning" style={{width: '16rem',  textAlign: 'center'}}>
     <Card.Header>Tổng số tác vụ bị bỏ qua</Card.Header>
     <Card.Body>
       <Card.Title>342</Card.Title>
@@ -31,12 +35,144 @@ export const Statistics = () => {
   </Card>
   <br />
 
-  <Card border="info" style={{ width: '18rem' }}>
+  <Card bg="info" style={{width: '16rem',  textAlign: 'center'}}>
     <Card.Header>Tổng số nhãn dán hoàn thành</Card.Header>
     <Card.Body>
       <Card.Title>976865</Card.Title>
     </Card.Body>
   </Card>
+  </section>
+  <br />
+  <br />
+  <section className="table">
+  <h5 style={{fontFamily: 'Inter', fontWeight: '700'}}> THỐNG KÊ TỪNG THÀNH VIÊN </h5>
+  <Table responsive="sm">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+    </tbody>
+  </Table>
+  <Table responsive="md">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+    </tbody>
+  </Table>
+  <Table responsive="lg">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+    </tbody>
+  </Table>
+  </section>
   </div>
   );
 };
