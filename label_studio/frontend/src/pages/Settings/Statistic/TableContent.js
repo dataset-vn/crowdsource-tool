a0a9c8d94c3f4e5d669200cefcb1a6f1283b1e0a
Huynh.nguyen <>
2022-06-26 15:36:31 +0700

    component

---
+++ label_studio/frontend/src/pages/Settings/Statistic/TableContent.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
+
+export const TableContent = React.memo(() => {
+  const [startDate, setStartDate] = React.useState(
+    new Date().toLocaleDateString("fr-CA"),
+  );
+  const [endDate, setEndDate] = React.useState(
+    new Date().toLocaleDateString("fr-CA"),
+  );
+  return (
+    <div className="right-content">
+      <div className="title">Instance / Day</div>
+      <div className="content">
+        <div className="filter">
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            className="input-search"
+            placeholder="Tìm kiếm ..."
+          />
+          <div className="filter-right">
+            <input
+              className="calendar-statistic"
+              name="project_due"
+              id="project_due"
+              type="date"
+              data-date=""
+              data-date-format="YYYY-MM-DD"
+              required
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+            {"  "} ~ {"  "}
+            <input
+              className="calendar-statistic"
+              name="project_due"
+              id="project_due"
+              type="date"
+              data-date=""
+              data-date-format="YYYY-MM-DD"
+              required
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Số nhãn dán</th>
+              <th>Số nhãn review</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>huynhphoke@gmail.com</td>
+              <td>300</td>
+              <td>100</td>
+            </tr>
+
+            <tr>
+              <td>huynhphoke@gmail.com</td>
+              <td>30</td>
+              <td>100</td>
+            </tr>
+            <tr>
+              <td>huynhphoke@gmail.com</td>
+              <td>30</td>
+              <td>100</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+});
